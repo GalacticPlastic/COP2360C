cbb233beb2f76c59697665ea7247c9bafad2d3f8
Mary Mederos <MaryMed@valleyforge.com>
2021-04-07 20:11:10 -0400

    Created Windows Form and Class.

--- InventoryManagementForm/Form1.cs
+++ InventoryManagementForm/Form1.cs
@@ -17,5 +17,59 @@ namespace InventoryManagementForm
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabViewProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
